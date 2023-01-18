2fea182e79fd10f58b6ac0ff272fe9e7d9024d20
koszav <zavos@tuta.io>
2023-01-18 17:11:17 +0200

    Format js document using prettier (ctrl+shift+i)

--- javascript.js
+++ javascript.js
@@ -1,39 +1,39 @@
 let myLibrary = [];
 
 function Book(bookName, bookAuthor) {
-    this.bookName = bookName
-    this.bookAuthor = bookAuthor
+  this.bookName = bookName;
+  this.bookAuthor = bookAuthor;
 }
 
 function addBookToLibrary() {
   bookName = prompt("What is the bookname?");
   bookAuthor = prompt("Who is the author?");
   const userInput = new Book(bookName, bookAuthor);
-  let pushToArray = myLibrary.push(userInput);
+  const pushToArray = myLibrary.push(userInput);
   return pushToArray;
 }
 
 function loopThroughtBooks() {
-  for (let Book of myLibrary) {
-    // Creates a new card 
-    const container = document.querySelector('.main-container');
-    const card = document.createElement('div');
-    card.classList.add('card');
+  for (const Book of myLibrary) {
+    // Creates a new card
+    const container = document.querySelector(".main-container");
+    const card = document.createElement("div");
+    card.classList.add("card");
     container.appendChild(card);
 
     // Creates name div
-    const bookNameCard = document.createElement('div');
-    bookNameCard.classList.add('book-name');
+    const bookNameCard = document.createElement("div");
+    bookNameCard.classList.add("book-name");
     bookNameCard.textContent = `Book name:${Book.bookName}`;
     card.appendChild(bookNameCard);
 
     // Creates author div
-    const bookAuthorCard = document.createElement('div');
-    bookAuthorCard.classList.add('book-author');
+    const bookAuthorCard = document.createElement("div");
+    bookAuthorCard.classList.add("book-author");
     bookAuthorCard.textContent = `Book author:${Book.bookAuthor}`;
     card.appendChild(bookAuthorCard);
   }
-return myLibrary = [];
+  return (myLibrary = []);
 }
 
 // addBookToLibrary();
